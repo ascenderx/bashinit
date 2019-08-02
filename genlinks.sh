99d56509c7c52019f42aee8219effa5c0a9d3df6
James D. Downer <downer.james.256k@gmail.com>
2019-08-02 13:49:49 -0600

    Replace and repurpose genlinks for conditionals

---
+++ genlinks.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Check command-line flags.
+FORCED=
+for arg in "$*"
+do
+  case $arg in
+    *-f*) FORCED="y" ;;
+  esac
+done
+
+# Uncomment the files you want to link.
+FILES=( \
+  "bashfmt" \
+  "bash_profile" \
+  "bashrc" \
+  "bashutils" \
+  "lscolors-linux" \
+#  "lscolors-bsd" \
+  "profile" \
+  "vimrc" \
+  "Xresources" \
+)
+
+# Iterate through the files and link them.
+for f in ${FILES[@]}
+do
+  LINK="$HOME/.$f"
+  
+  # Check to see if link/file already exists.
+  if [[ -L $LINK ]] || [[ -f $LINK ]] 
+  then
+    # Confirm existing file replacement if -f flag was not set.
+    if [[ -z $FORCED ]]
+    then
+      echo -n "Replace $LINK? [y/N] "
+      read val
+      case $val in
+        *y*) ;&
+        *Y*) ;;
+        *) continue ;;
+      esac
+    fi
+    
+    rm -f $LINK
+  fi
+  
+  # Generate the symbolic link.
+  echo ":: $PWD/$f -> $LINK"
+  ln -s $PWD/$f $LINK
+done
